304acf0c8b394fa2fd465c76d4b225b124d2a771
yangxuan8282 <yangxuan8282@gmail.com>
2017-06-18 05:21:33 +0800

    add typecho php image

---
+++ typecho/dev/php/armhf/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+if [ "$1" = 'php' ] && [ "$(id -u)" = '0' ]; then
+    chown -R www-data /var/www/html
+    chmod -R 777 /var/www/html/
+fi
+
+if [ ! -e '/var/www/html/index.php' ]; then
+    cp -a /usr/src/typecho/* /var/www/html/
+    chown -R www-data /var/www/html
+    chmod -R 777 /var/www/html/
+fi
+
+exec "$@"
